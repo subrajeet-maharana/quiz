0d39e3008151ccd47e1f4181bd1b33139502d95f
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-06-03 10:47:37 +0530

    addressed issue #3

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ type questionAnswer struct {
 }
 
 func main() {
-	//opening file in golang
 	strPtr := flag.String("filename", "problems.csv", "CSV File Name")
 	// intPtr := flag.Int("time", 30, "Time Limit")
 	flag.Parse()
@@ -28,29 +27,22 @@ func main() {
 	checkNilErr(err)
 	defer f.Close()
 
-	// reading from csv file
 	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll() //data will give [][]string
+	data, err := csvReader.ReadAll()
 	checkNilErr(err)
 
-	// printing data from csv
-	// number of rows --> len(data) and columns --> len(data[0])
-	var val string
 	var score int = 0
-	for i := 0; i < len(data); i++ {
-		for j := 0; j < 1; j++ {
-			fmt.Printf("%d. %s = ", i+1, data[i][j])
+	for i, row := range data {
+		fmt.Printf("%d. %s = ", i+1, row[0])
+		var val string
+		fmt.Scan(&val)
+		if val == row[1] {
+			score++
 		}
-		for j := 0; j < 1; j++ {
-			fmt.Scan(&val)
-			if val == data[i][1] {
-				score++
-			}
-		}
-		fmt.Printf("\n")
 	}
 	fmt.Printf("You have scored %d out of %d", score, len(data))
 }
+
 func checkNilErr(err error) {
 	if err != nil {
 		log.Fatal(err)
