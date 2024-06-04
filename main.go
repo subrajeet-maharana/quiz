dbdc6e41368d5bba025de01f531c9d5011abb528
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-06-04 17:50:44 +0530

    working fine with single line input

--- main.go
+++ main.go
@@ -7,72 +7,68 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
-
-	"github.com/gocarina/gocsv"
+	"strings"
 )
 
 type QuestionAnswer struct {
-	question string
-	answer   string
+	Question string
+	Answer   string
 }
 
 func main() {
 	strPtr := flag.String("filename", "problems.csv", "CSV File Name")
 	flag.Parse()
-	var commandIs string
+	var commandIs string = *strPtr
 	if *strPtr != "problems.csv" {
 		commandIs = *strPtr + ".csv"
-	} else {
-		commandIs = *strPtr
 	}
+
 	f, err := os.Open(commandIs)
 	checkNilErr(err)
 	defer f.Close()
+
+	stat, err := f.Stat()
+	checkNilErr(err)
+	if stat.Size() == 0 {
+		log.Fatal("empty CSV file given")
+	}
 
 	csvReader := csv.NewReader(f)
 	data, err := csvReader.ReadAll()
 	checkNilErr(err)
 
-	var questions []QuestionAnswer
-
-	for _, d := range data[1:] {
-		var q QuestionAnswer
-		populateStructFromCSV(d, &q)
-		questions = append(questions, q)
+	if len(data) == 0 {
+		log.Fatal("no questions found in the CSV file")
 	}
 
-	questionAnswers := []*QuestionAnswer{}
-	if err := gocsv.UnmarshalFile(f, &questionAnswers); err != nil {
-		panic(err)
+	var questionAnswers []QuestionAnswer
+	for _, row := range data {
+		if len(row) != 2 {
+			log.Fatal("invalid CSV format: each row must contain exactly two columns")
+		}
+		questionAnswers = append(questionAnswers, QuestionAnswer{
+			Question: row[0],
+			Answer:   row[1],
+		})
 	}
-	var score int = 0
+
+	var score int
+	reader := bufio.NewReader(os.Stdin)
 	for i, q := range questionAnswers {
-		fmt.Printf("%d. %s ", i+1, q.question)
-		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("%d. %s ", i+1, q.Question)
 		val, err := reader.ReadString('\n')
 		checkNilErr(err)
-		val = val[:len(val)-1]
-		fmt.Printf("%s %s\n", val, q.answer)
-		if val == q.answer {
+		val = strings.TrimSpace(val)
+		if val == q.Answer {
 			score++
 		}
 	}
-	fmt.Printf("You have scored %d out of %d", score, len(data))
+
+	fmt.Printf("You have scored %d out of %d\n", score, len(questionAnswers))
 }
 
 func checkNilErr(err error) {
 	if err != nil {
 		log.Fatal(err)
-	}
-}
-
-func populateStructFromCSV(data []string, result interface{}) {
-	val := reflect.ValueOf(result).Elem()
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		if field.CanSet() {
-			field.SetString(data[i])
-		}
 	}
 }
